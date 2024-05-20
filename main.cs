f99ab0e280de73b650a9c9de686389b635b1f5b6
aoyola6 <116612522+aoyola6@users.noreply.github.com>
2024-05-20 16:16:42 +0000

    Unit Test 2 Commit

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Oh, well imagine /");
-    Console.WriteLine("As I'm pacing the pews in a church corridor /");
-    Console.WriteLine("And I can't help but to hear /");
-    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+    Console.WriteLine ("Oh-oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh /");
+    Console.WriteLine("Caught in a bad romance...");
   }
 }
