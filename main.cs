4a02371b7b018359494fa188db55d33ac8fd8734
aoyola6 <116612522+aoyola6@users.noreply.github.com>
2024-05-20 16:07:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Oh, well imagine /");
+    Console.WriteLine("As I'm pacing the pews in a church corridor /");
+    Console.WriteLine("And I can't help but to hear /");
+    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+  }
+}
